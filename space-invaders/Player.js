d5bebcc8e855b6d3aafe610eea14d9db5c858c6e
Gautam Gupta <ggupta78@gmail.com>
2022-11-05 18:42:38 +0800

    complete player bulletController

--- space-invaders/Player.js
+++ space-invaders/Player.js
@@ -1,10 +1,12 @@
 export default class Player {
   rightPressed = false;
   leftPressed = false;
+  shootPressed = false;
 
-  constructor(canvas, velocity) {
+  constructor(canvas, velocity, bulletController) {
     this.canvas = canvas;
     this.velocity = velocity;
+    this.bulletController = bulletController;
 
     this.x = this.canvas.width / 2;
     this.y = this.canvas.height - 75;
@@ -18,6 +20,9 @@ export default class Player {
   }
 
   draw(ctx) {
+    if (this.shootPressed) {
+      this.bulletController.shoot(this.x + this.width / 2, this.y, 4, 10);
+    }
     this.move();
     ctx.drawImage(this.image, this.x, this.y,
                   this.width, this.height);
@@ -48,6 +53,9 @@ export default class Player {
     if (event.code === "ArrowLeft") {
       this.leftPressed = true;
     }
+    if (event.code === "Space") {
+      this.shootPressed = true;
+    }
   };
 
   keyUp = event => {
@@ -56,6 +64,9 @@ export default class Player {
     }
     if (event.code === "ArrowLeft") {
       this.leftPressed = false;
+    }
+    if (event.code === "Space") {
+      this.shootPressed = false;
     }
   };
 }
--- space-invaders/index.js
+++ space-invaders/index.js
@@ -1,3 +1,4 @@
+import BulletController from "./BulletController.js";
 import EnemyController from "./EnemyController.js";
 import Player from "./Player.js";
 
@@ -10,13 +11,15 @@ canvas.height = 600;
 const background = new Image();
 background.src = "/images/space.png";
 
+const playerBulletController = new BulletController(canvas, 10, "red", true);
 const enemyController = new EnemyController(canvas);
-const player = new Player(canvas, 3);
+const player = new Player(canvas, 3, playerBulletController);
 
 function game() {
   ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
   // enemyController.draw(ctx);
   player.draw(ctx);
+  playerBulletController.draw(ctx);
 }
 
 setInterval(game, 1000 / 60);
